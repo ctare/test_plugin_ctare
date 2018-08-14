130b2a71da93e848df4a343008b9be2ab1bb01fa
ctare <sankaku900622@yahoo.co.jp>
2018-08-15 00:48:00 +0900

    first

---
+++ rplugin/python3/test_plugin.py
@@ -0,0 +1,20 @@
+import neovim
+
+@neovim.plugin
+class TestPlugin:
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @neovim.function("TestFunction", sync=True)
+    def test_function(self, args):
+        return 3
+
+    @neovim.command("TestCommand", range='', nargs='*')
+    def test_command(self, args, rng):
+        self.nvim.current.line = ('Command with args: {}, range: {}'.format(args, rng))
+
+    @neovim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")', sync=True)
+    def on_bufenter(self, filename):
+        self.nvim.out_write("testplugin is in {} \n".format(filename))
+
+
